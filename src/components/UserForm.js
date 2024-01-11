fcc38f1241311f17f4622c0a48cb359e7b047e3e
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-11 20:56:05 +0530

    test1

---
+++ src/components/UserForm.js
@@ -0,0 +1,91 @@
+import React, { useContext, useEffect, useState } from "react";
+import UserContext from "../Context/UserContext";
+
+const UserForm = () => {
+  const [localMsg, setLocalMsg] = useState("");
+  const { user, setUser } = useContext(UserContext);
+
+  //   useEffect(() => {
+  //     console.log(user);
+  //     console.log(user.listArr.length);
+  //   }, [user]);
+
+  //   useEffect(() => {
+  //     console.log(localMsg);
+  //   }, [localMsg]);
+  //
+  const handleAdd = () => {
+    if (user.isAuthenticated) {
+      setUser({
+        ...user,
+        listArr: [
+          ...user.listArr,
+          { id: user.listArr.length === 0 ? 0 : user.listArr[user.listArr.length - 1].id + 1, localMsg: localMsg },
+        ],
+      });
+      setLocalMsg("");
+    } else {
+      alert("User is not Logged in. Please log in first");
+    }
+  };
+
+  const handleClear = () => {
+    if (user.isAuthenticated) {
+      setUser({ ...user, listArr: [] });
+    } else {
+      alert("User is not Logged in. Please log in first");
+    }
+  };
+
+  const handleRemove = (removeItem) => {
+    setUser({...user, listArr : user.listArr.filter(item => item.id !== removeItem.id)})
+  };
+  //
+  return (
+    <div>
+      <h5 id="current-user">
+        Current user: {user.name},isAuthenticated: {`${user.isAuthenticated}`}
+      </h5>
+      <form onSubmit={(e) => {e.preventDefault();}}>
+        
+        <button 
+          id="login-btn"
+          onClick={() =>setUser({ ...user, isAuthenticated: true, name: "rohan" })}
+        > Login </button>
+        
+        <button
+          id="signout"
+          onClick={() => setUser({ ...user, isAuthenticated: false, name: "" })}
+        > SignOut </button>
+        
+        <input
+          id="shopping-input"
+          type="text"
+          placeholder="enter text"
+          style={{ display: "block" }}
+          onChange={(e) => setLocalMsg(e.target.value)}
+          value={localMsg}
+        />
+
+        <button onClick={() => handleAdd()}>Add</button>
+        <button id="clear-list" onClick={() => handleClear()}>Clear List</button>
+
+        <div>
+          {
+            user.listArr.length !== 0 && user.listArr.map((item) => (
+                <div>
+                    <button
+                      id={`remove-${item.localMsg}`}
+                      onClick={() => handleRemove(item)}
+                    > Remove </button>
+                    <span id={`item-${item.localMsg}`}> {item.localMsg} </span>
+                </div>
+              ))
+          }
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import UserProvider from "./Context/UserProvider";
 
-
+ReactDOM.render(
+  <UserProvider>
+    <App />
+  </UserProvider>,
+  document.getElementById("root")
+);
 
